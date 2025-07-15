cbbeaaa30a736ac0882949ea4ecbed2e75db2826
PerfectSkillsSophia <tech@perfectskills.in>
2025-07-15 20:13:59 +0530

    whitenoise and seetings

--- assessment/urls.py
+++ assessment/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', views.Assessments_Dashboard, name='Assessments_Dashboard'),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('assessment/begin/<str:assessment_code>/upload/', views.upload_recording, name='upload_recording'),
     path('feedback/', views.feedback_page, name='feedback_page'),
 ]
+
+if settings.DEBUG or True:  # For production, you might want to handle this differently
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
