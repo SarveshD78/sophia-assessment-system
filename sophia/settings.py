3ed6b502fc8911461464ab0d84af9d767bf382ff
PerfectSkillsSophia <tech@perfectskills.in>
2025-07-15 20:18:01 +0530

    whitenoise and seetings

--- sophia/settings.py
+++ sophia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tq+aa!r#0(mw9a^*g&rt9gyn+e57wrns9#i095(dv7*vc!g=#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
